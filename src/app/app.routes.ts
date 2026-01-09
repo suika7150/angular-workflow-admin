5fa01584919ec5248892cbb00470cdff1367d76a
suika7150 <151140029+suika7150@users.noreply.github.com>
2026-01-09 17:24:36 +0800

    2026/01/09下班前紀錄點 dashboard待完成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { TaskListComponent } from ',/components/task-list/task-list.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
